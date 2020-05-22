216127c50db80ed757f2a4b6fa20efe7f455f306
Tehila Chasidim <tehilahchasidim@gmail.com>
2020-05-22 17:17:43 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,171 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <div class="mobile-nav-block">
+      <div class="icon">
+        <img src="@/assets/T.png">
+      </div>
+      <span class="mobile-menu-icon" @click="openMobileNav()"><i class="material-icons">menu</i></span>
+    </div>
+    <div class="mobile-nav">
+      <router-link to="/" title="Home"><i class="material-icons">home</i></router-link> 
+      <router-link to="/" title="About"><i class="material-icons">person</i></router-link> 
+      <router-link to="/" title="Skills"><i class="material-icons">work</i></router-link> 
+      <router-link to="/" title="My work"><i class="material-icons">visibility</i></router-link> 
+      <router-link to="/" title="Contact"><i class="material-icons">email</i></router-link> 
       <router-link to="/about">About</router-link>
+    </div>
+    <div class="nav">
+      <div class="icon">
+        <img src="@/assets/T.png">
+      </div>
+      <div class="nav-items">
+        <router-link to="/" title="Home"><i class="material-icons">home</i></router-link> 
+        <router-link to="/" title="About"><i class="material-icons">person</i></router-link> 
+        <router-link to="/" title="Skills"><i class="material-icons">work</i></router-link> 
+        <router-link to="/" title="My work"><i class="material-icons">visibility</i></router-link> 
+        <router-link to="/" title="Contact"><i class="material-icons">email</i></router-link> 
+        <router-link to="/about">About</router-link>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  methods: {
+      openMobileNav: function(){
+        let $ = require('jquery');
+        $(".mobile-nav").animate({width:'toggle'});     
+      }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  html{
+    overflow: hidden;
+  }
+  
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .mobile-nav{
+    display: block;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .nav {
+    background-color: #181818;
+    height: 5rem;
+    display: none;
+    position: absolute;
+    width: 5rem;
+    height: 100%;
+  }
+
+  .nav-items{
+    margin-top: 5rem;
+  }
+
+  .nav-items a {
+    font-weight: bold;
+    color: #969494;
+  }
+
+  .nav .icon{
+    background-color: black;
+    padding: 1rem;
+  }
+
+  .nav .icon img{
+    width: 100%;
+    height: 100%;
+  }
+
+  .nav-items a.router-link-exact-active, .mobile-nav a.router-link-exact-active{
+    color: #00c3ff
+  }
+
+  .mobile-nav-block{
+    height: 5rem;
+    background-color: #181818;
+    display: flex;
+  }
+
+  .mobile-nav-block .icon{
+    background-color: black;
+    width: 5rem;
+    display: inline-block;
+    padding: 1rem;
+  }
+
+  .mobile-nav-block .icon img{
+    width: 100%;
+    height: 100%;
+  }
+
+  .mobile-nav{
+    background-color: #181818;
+    position: absolute;
+    z-index: 1;
+    display: none;
+    top: 5rem;
+    width: 100%;
+    height: 4rem;
+  }
+
+  .mobile-nav a{
+    color: #969494;
+    width: 20%;
+    float: left;
+  }
+
+  .mobile-menu-icon{
+    width: 3rem;
+    height: 3rem;
+    right: 1rem;
+    margin: 1.7rem 0rem;
+    position: absolute;
+    cursor: pointer;
+    color: white;
+    display: block;
+    width: fit-content;
+  }
+
+  .mobile-menu-icon i{
+    font-size: 2.5rem;
+  }
+
+  .mobile-nav a i, .nav a i{
+    font-size: 2rem;
+    margin: 1rem;
+  }
+
+  @media (min-width: 40rem)
+  {
+    .mobile-menu-icon{
+      display: none;
+    }
+
+    .mobile-nav-block{
+      display: none;
+    }
+
+    .mobile-nav{
+      display: none;
+      visibility: hidden;
+    }
+
+    .nav{
+      display: block;
+    }
+  }
+  
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,178 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="icon">
+      <p class="blue-T">T</p>
+      <p class="pink-T">T</p>
+    </div>
+    <div class="home-front">
+      <div class="discription">
+        <p>Hi,</p>
+        <p>I'm Tehila,</p>
+        <p>front end developer.</p>
+      </div>
+    </div>
+    <button>Resume</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+<style scoped>
+
+  .home{
+    min-height: calc(100vh - 5rem);
+    background-color: #1f1f1f;
+  }
+
+  .icon p{
+    width: fit-content;
+    position: absolute;
+    font-size: 28rem;
+    transform: rotate(340deg);
+    font-family: sans-serif;
+  }
+
+  .blue-T{
+    color: #00c3ff;
+    right: -3.2rem;
+    top: 4.7rem;
+  }
+
+  .pink-T{
+    color: #f456d5;
+    right: -2.2rem;
+    top: 6rem;
+  }
+
+  .home-front{
+    position: relative;
+  }
+
+  .discription{
+    color: white;
+    font-size: 2rem;
+    padding: 2rem;
+    padding-top: 6rem;
+  }
+
+  .discription p{
+    text-align: left;
+    font-size: 3rem;
+    font-family: 'Lobster', cursive;
+    padding-left: 1rem;
+    white-space: nowrap;
+    overflow: hidden;
+    width: 100%;
+    display: flex;
+    opacity: 0;
+    -webkit-animation: type2 2s steps(40, end);
+    animation: type2 2s steps(40, end);
+    -webkit-animation-delay: 2s;
+    animation-delay: 2s;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+  }
+
+  .discription p:nth-child(3){
+    white-space: normal;
+  }
+
+  @keyframes type2 {
+    0% {
+      width: 0;
+    }
+    1% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 1;
+      border: none;
+    }
+  }
+
+  @-webkit-keyframes type2 {
+    0% {
+      width: 0;
+    }
+    1% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 1;
+      border: none;
+    }
+  }
+
+   button{
+    cursor: pointer;
+    position: absolute;
+    z-index: 1;
+    left: 2rem;
+    font-size: 1.5rem;
+    color: #00c3ff;
+    background-color: transparent;
+    border: 2px solid #00c3ff;
+    padding: 0.5rem;
+    border-radius: 3px;
+  }
+
+  button:hover{
+    color: #f456d5;
+    background-color: transparent;
+    border: 2px solid #f456d5;
+  }
+
+  @media (min-width: 27rem)
+  {
+    .discription p:nth-child(3){
+      white-space: nowrap;
+    }
+  }
+
+  @media (min-width: 40rem)
+  {
+    .home{
+      min-height: calc(100vh);
+    }
+
+    .home-front {
+      left: 9rem;
+      top: 4rem;
+    }
+
+    .icon p{
+      font-size: 40rem;
+      margin-right: 15%;
+    }
+
+    .blue-T{
+      top: -2rem;
+    }
+
+    .pink-T{
+      top: 0;
+    }
+
+    .discription{
+      padding-left: 0;
+    }
+
+    .discription p{
+      font-size: 4rem;
+    }
+
+    button{
+      top: 27rem;
+      left: 11rem;
+      font-size: 1.7rem;
+    }
+  }
+
+</style>
